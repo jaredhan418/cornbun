8809b9779d75841586a268dde043258272bf80f2
jaredhan <hanjiawei@outlook.com>
2024-10-20 22:47:02 +0800

    Add lock api.

---
+++ app/api/command/lock/route.ts
@@ -0,0 +1,27 @@
+import { auth } from "@/auth";
+import { prisma } from "@/prisma";
+import { fetchCommandApi } from "@/app/serviceUtil";
+
+export async function GET(request: Request) {
+  const session = await auth();
+
+  const { searchParams } = new URL(request.url)
+
+  const userId = session?.user?.id;
+
+  const account = await prisma.account.findFirst({ where: {
+    userId
+  }})
+
+  const accessToken = account?.access_token;
+
+  if (!accessToken) {
+    throw new Error('Not has token')
+  }
+
+  searchParams.append("token", accessToken);
+
+  await fetchCommandApi("lock", { searchParams }).json();
+
+  return new Response("Lock!");
+}
